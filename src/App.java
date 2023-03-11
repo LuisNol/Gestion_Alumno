f531d1d5dd70ad9ee739a94357ffb772861a690c
igor <igor.ramos.c@hotmail.com>
2023-03-11 10:34:48 -0500

    estructura de archivos

--- src/App.java
+++ src/App.java
@@ -1,10 +1,9 @@
-package main;
 import controlador.ControladorAlumno;
 import modelo.Alumno;
 import vista.AlumnoUI;
 
-public class Main {
-    public static void main(String[] args) {
+public class App {
+    public static void main(String[] args) throws Exception {
         Alumno alumno= new Alumno();
         ControladorAlumno controladorAlumno = new ControladorAlumno(alumno);
         controladorAlumno.generarNotaParciales(10);
