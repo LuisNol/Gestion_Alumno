307a107527597b643114993d0e7dd600893bc117
igor <igor.ramos.c@hotmail.com>
2023-03-23 12:33:46 -0500

    feat/new atributes version 1

--- src/modelo/Examen.java
+++ src/modelo/Examen.java
@@ -2,4 +2,23 @@ package modelo;
 
 public class Examen {
     
+    private String nombreExmane;
+    private float puntaje;
+
+    public void setNombreExmane(String nombreExmane) {
+        this.nombreExmane = nombreExmane;
+    }
+
+    public void setPuntaje(float puntaje) {
+        this.puntaje = puntaje;
+    }
+
+    public String getNombreExmane() {
+        return nombreExmane;
+    }
+
+    public float getPuntaje() {
+        return puntaje;
+    }
+
 }
