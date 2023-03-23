2d4acc7bae59d0df2aa50c78f33442020b756aee
igor <igor.ramos.c@hotmail.com>
2023-03-23 08:28:16 -0500

    feat/ new atribute cedulaDOcente  in the Class Person and functions get set

--- src/modelo/Docente.java
+++ src/modelo/Docente.java
@@ -3,6 +3,7 @@ public class Docente extends Persona {
     private String area;
     private String especialidad;
     private String gradoAcademico;
+    private int cedulaDocente;
     
     public Docente(String nombre, int edad, String genero, String direccion, String telefono, String area, String especialidad, String gradoAcademico) {
         super(nombre, edad, genero, direccion, telefono);
@@ -10,8 +11,24 @@ public class Docente extends Persona {
         this.especialidad = especialidad;
         this.gradoAcademico = gradoAcademico;
     }
-    
+
+    public Docente(int cedulaDocente,String nombre, int edad, String genero, String direccion, String telefono, String area, String especialidad, String gradoAcademico){
+        super(nombre,edad,genero,direccion,telefono);
+        this.cedulaDocente = cedulaDocente;
+        this.especialidad = especialidad;
+        this.gradoAcademico = gradoAcademico;
+    }
+
     // Getters y Setters
+
+    public int getCedulaDocente() {
+        return cedulaDocente;
+    }
+    
+    public void setCedulaDocente(int cedulaDocente) {
+        this.cedulaDocente = cedulaDocente;
+    }
+
     public String getArea() {
         return area;
     }
